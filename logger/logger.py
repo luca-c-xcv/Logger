4eef59a21b2dc8adc01ef731658027f4a9f4ab8b
ＬＶＣ Λ <lccanessa@gmail.com>
2024-05-02 22:55:03 +0200

    adoption of the singleton pattern
    To keep the system simple and easy to maintain, the logger has been
    redesigned as a singleton, so that only one instance can be used even if
    it is called by different methods.

--- logger/logger.py
+++ logger/logger.py
@@ -1,9 +1,18 @@
 import logging
 
 class Logger:
-    def __init__(self, log_path='./crawler.log'):
+    _logger = None
+
+    def __init__(self, log_path):
         logging.basicConfig(filename=log_path, level=logging.DEBUG,format='%(asctime)s [%(levelname)s] ::: %(message)s')
         self.logger = logging.getLogger(__name__)
+
+    @classmethod
+    def init(cls, log_path='/tmp/logger.log'):
+        if not cls._logger:
+            cls._logger = cls(log_path)
+        return cls._logger
+
     def _log_message(self, message, level=logging.DEBUG):
         """ define the type of log message """
         self.logger.log(level, message)
